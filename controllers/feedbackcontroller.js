ac32fd19e11f26bbe3b54eb608d28a50a1df1463
Emanuel Pires <emanuelpires11@gmail.com>
2019-06-05 17:43:47 -0400

    Testing

--- controllers/feedbackcontroller.js
+++ controllers/feedbackcontroller.js
@@ -21,11 +21,11 @@ module.exports = {
   tweet: function(req, res) {
     console.log('Why is this not working');
     console.log('**********************************');
-    console.log(req.body.base64);
+    console.log(JSON.stringify(req.body));
     console.log('**********************************');
     // console.log(JSON.stringify(req.body));
     console.log('**********************************');
-    T.post('media/upload', { media_data: JSON.stringify(req.body) }, function(
+    T.post('media/upload', { media_data: req.body }, function(
       err,
       data,
       response
