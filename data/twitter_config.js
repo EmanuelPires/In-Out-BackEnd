25ef48a7aa9116470930fbeafb2d6991aba64224
Emanuel Pires <emanuelpires11@gmail.com>
2019-06-12 18:44:38 -0400

    Update twitter_config.js

--- data/twitter_config.js
+++ data/twitter_config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: 'PWq3rwT5V8gVzyEyHun2s7LYV',
-  consumer_secret: 'XBlDN7uLCQSiACHXbSPVJibkXVty4W6j6d0vguoVnuUVky93pL',
-  access_token: '954772705403899910-NwTW9aA0ZfrXj1nrGbdtYrqL8g6N3Ze',
-  access_token_secret: 'yLmq8LLeeodwwjjDdWQdPLGa0iojS49PeeIzn2mV3FrO0'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: ''
 };
